85093fcf9c427bbec5a095325ca2004206f786cb
AlessiaGava <74045428+AlessiaGava@users.noreply.github.com>
2020-11-26 10:14:17 +0100

    Insert the function to load the excel file

---
+++ Main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Carico il file excel
+dataframe_volley = pd.read_excel('C:/Users/Alessia/Desktop/HFARM/LAB OF SOFTWARE PROJECT DEVELOPMENT/DATASET VOLLEY.xlsx')
+#print(dataframe_volley)
